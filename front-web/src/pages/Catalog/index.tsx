c9f72c474ee6b51a593208d9d122587e62bad2c7
Gleisson Santos Silva <gtsasil@gmail.com>
2021-01-29 21:19:59 +0000

    Product listing layout finished

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import ProductCard from "./components/ProductCard";
+import "./styles.scss";
 
 const Catalog = () => (
-    <div>
-        <h1>
-            Catalog
-        </h1>
+  <div className="catalog-container">
+    <h1 className="catalog-title">Products Catalog</h1>
+    <div className="catalog-products">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </div>
+  </div>
+);
 
- );
-
- export default Catalog;
+export default Catalog;
