7c99865af181d17c1b10d09704505bf7864394d3
Gleisson Santos Silva <gtsasil@gmail.com>
2021-02-20 11:38:15 +0000

    End of backend part 2.

--- backend/src/main/java/com/gtsasil/DevSuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/gtsasil/DevSuperior/dscatalog/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.gtsasil.DevSuperior.dscatalog.repositories;
 
+import com.gtsasil.DevSuperior.dscatalog.entities.Category;
 import com.gtsasil.DevSuperior.dscatalog.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE " +
+            "(:category IS NULL OR:category IN cats) AND " +
+            "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
+    Page<Product> find(Category category,String name, Pageable pageable);
+
 }
